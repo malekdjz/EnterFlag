b3c1b810b927b28547fc24e8026485de14e24d58
malekdjz <malekdjz@gmail.com>
2022-11-12 00:42:45 +0100

    added place holders

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+class Index(View):
+    pass
+
+class Login(View):
+    pass
+
+class Register(View):
+    pass
+
+class Profile(View):
+    pass
+
+class TeamDetail(View):
+    pass
+
+class TeamList(View):
+    pass
+
+class ChallangeDetail(View):
+    pass
+
+class ChallangeList(View):
+    pass
+
+class About(View):
+    pass
+
+class Dashboard(View):
+    pass
