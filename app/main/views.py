245e4f2e482748df070f8b1f420812bddca17144
malekdjz <malekdjz@gmail.com>
2022-11-19 20:25:32 +0100

    small progress

--- app/main/views.py
+++ app/main/views.py
@@ -1,33 +1,76 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from main.models import *
 # Create your views here.
 
 class Index(View):
-    pass
+    def get(self,request):
+        return render(request,'index.html')
 
 class Login(View):
-    pass
+    def get(self,request):
+        return render(request,'auth/login.html')
+    def post(self,request):
+        return
+
+class Logout(View):
+    def post(self,request):
+        return
 
 class Register(View):
-    pass
+    def get(self,request):
+        return render(request,'auth/register.html')
+
+    def post(self,request):
+        return
 
 class Profile(View):
-    pass
+    def get(self,request,username):
+        profile = User.objects.get(username=username)
+        return render(request,'user/profile.html')
+    
+    def put(slef,request):
+        return
 
 class TeamDetail(View):
-    pass
+    def get(self,request,name):
+        team = Team.objects.get(name=name)
+        return render(request,'event/team-detail.html')
+    
+    def put(self,request):
+        return
 
 class TeamList(View):
-    pass
+    def get(self,request):
+        teams = Team.objects.all()
+        resultss = Paginator(teams,50)
+        return render(request,'event/team-list.html')
+    
+    def post(self,request):
+        return
 
 class ChallangeDetail(View):
-    pass
+    def get(self,request,name):
+        challange = Challange.objects.get(name=name)
+        return render(request,'event/challange-detail.html')
+    
+    def put(self,request,name):
+        return
 
 class ChallangeList(View):
-    pass
+    def get(self,request):
+        challanges = Challange.objects.all()
+        return render(request,'event/challange-list.html')
+    
+    def post(self,request):
+        return
 
 class About(View):
-    pass
+    def get(self,request):
+        return render(request,'misc/about.html')
 
 class Dashboard(View):
-    pass
+    def get(self,request):
+        return render(request,'admin/dashboard.html')
