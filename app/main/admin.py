c083c435c33d6b38388f13eaa5919501c22e7ea1
malekdjz <malekdjz@gmail.com>
2022-11-19 20:25:22 +0100

    added models

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Event)
+admin.site.register(Challange)
+admin.site.register(Participant)
+admin.site.register(Category)
